dcdf6c8004a3d47bb7a1a925e17487c834ad5f8c
KovalVladyslav <vladxnos@gmail.com>
2023-05-02 15:03:33 +0300

    all 4 tasks done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from './Profile/Profile';
+import userData from '../user.json';
+import { Statistics } from './Statistics/statistics';
+import data from '../data.json';
+import { FriendList } from './Friendlist/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
-      React homework template
+      <Profile user={userData} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
